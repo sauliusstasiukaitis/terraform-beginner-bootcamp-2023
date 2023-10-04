bb530cfc461a550b9c6a953f6b46aab24c1ce924
Saulius Stasiukaitis <stasiukaitis.saulius@gmail.com>
2023-10-04 13:37:07 +0000

    Add variables for AWS in cloud

--- variables.tf
+++ variables.tf
@@ -12,3 +12,15 @@ variable "bucket_name" {
     type = string
     description = "AWS S3 bucket name"
 }
+
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+}
+
+variable "AWS_DEFAULT_REGION" {
+  type = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
+}

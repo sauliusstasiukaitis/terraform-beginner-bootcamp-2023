d106b62db7612f9b0a49725c62c4a2537e551216
Saulius Stasiukaitis <stasiukaitis.saulius@gmail.com>
2023-10-04 14:35:01 +0000

    Refactor: extract AWS S3 functionality to a bucket

---
+++ modules/terrahouse_aws/variables.tf
@@ -0,0 +1,14 @@
+variable "user_uuid" {
+    type        = string
+    description = "The UUID of the user"
+
+  validation {
+    condition     = can(regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$", var.user_uuid))
+    error_message = "user_uuid must be a valid UUID."
+  }
+}
+
+variable "bucket_name" {
+    type = string
+    description = "AWS S3 bucket name"
+}

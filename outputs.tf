abe67c05cb5a5b48353d8665265772bfdbf85355
Saulius Stasiukaitis <stasiukaitis.saulius@gmail.com>
2023-10-06 07:05:31 +0000

    Add output for cloudfront URL
    To have an easy accessible link to check that static site is working.

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "website_bucket" {
 output "s3_website_endpoint" {
     value = module.terrahouse_aws.website_endpoint
 }
+
+output "cloudfront_url" {
+    value = module.terrahouse_aws.cloudfront_url
+}

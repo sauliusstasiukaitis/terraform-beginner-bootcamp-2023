b70ad48aacb932d616493bdd2cbe54698ad9835a
Saulius Stasiukaitis <stasiukaitis.saulius@gmail.com>
2023-10-06 20:43:31 +0000

    Add use for terratown provider

--- main.tf
+++ main.tf
@@ -1,19 +1,32 @@
 terraform {
-    cloud {
-        organization = "sauliusstasiukaitis"
+    # cloud {
+    #     organization = "sauliusstasiukaitis"
 
-        workspaces {
-            name = "terra-house-1"
+    #     workspaces {
+    #         name = "terra-house-1"
+    #     }
+    # }
+
+    required_providers {
+        terratowns = {
+            source = "local.providers/local/terratowns"
+            version = "1.0.0"
         }
     }
 }
 
-module "terrahouse_aws" {
-    source = "./modules/terrahouse_aws"
-    user_uuid = var.user_uuid
-    bucket_name = var.bucket_name
-    index_html_file_path = var.index_html_file_path
-    error_html_file_path = var.error_html_file_path
-    content_version = var.content_version
-    assets_path = var.assets_path
+# module "terrahouse_aws" {
+#     source = "./modules/terrahouse_aws"
+#     user_uuid = var.user_uuid
+#     bucket_name = var.bucket_name
+#     index_html_file_path = var.index_html_file_path
+#     error_html_file_path = var.error_html_file_path
+#     content_version = var.content_version
+#     assets_path = var.assets_path
+# }
+
+provider "terratowns" {
+    endpoint = "http://localhost:4567/api"
+    user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
+    token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
-output "website_bucket" {
-    value = module.terrahouse_aws.website_bucket
-}
+# output "website_bucket" {
+#     value = module.terrahouse_aws.website_bucket
+# }
 
-output "s3_website_endpoint" {
-    value = module.terrahouse_aws.website_endpoint
-}
+# output "s3_website_endpoint" {
+#     value = module.terrahouse_aws.website_endpoint
+# }
 
-output "cloudfront_url" {
-    value = module.terrahouse_aws.cloudfront_url
-}
+# output "cloudfront_url" {
+#     value = module.terrahouse_aws.cloudfront_url
+# }

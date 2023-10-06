92442233268baa67ed44c576ada7147c47e72807
Saulius Stasiukaitis <stasiukaitis.saulius@gmail.com>
2023-10-06 07:14:34 +0000

    WIP: fix invalidate command
    Error: local-exec provisioner error
    with module.terrahouse_aws.terraform_data.invalidate_cache
    on modules/terrahouse_aws/resource-cdn.tf line 67, in resource "terraform_data" "invalidate_cache":
    Error running command '        aws cloudfront create-invalidation \
            --instribution-id E1XQALFXW6VAYM
            --paths '/*'
    ': exit status 127. Output: usage:
    Note: AWS CLI version 2, the latest major version of the AWS CLI, is now stable and recommended for general use. For more information, see the AWS CLI version 2 installation instructions at: https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2.html
    
    usage: aws [options] <command> <subcommand> [<subcommand> ...] [parameters]
    To see help text, you can run:
    
      aws help
      aws <command> help
      aws <command> <subcommand> help
    aws: error: the following arguments are required: --distribution-id
    /bin/sh: 3: --paths: not found

--- modules/terrahouse_aws/resource-cdn.tf
+++ modules/terrahouse_aws/resource-cdn.tf
@@ -65,10 +65,6 @@ resource "terraform_data" "invalidate_cache" {
     triggers_replace = terraform_data.content_version.output
 
     provisioner "local-exec" {
-        command = <<COMMAND
-        aws cloudfront create-invalidation \
-        --instribution-id ${aws_cloudfront_distribution.s3_distribution.id}
-        --paths '/*'
-        COMMAND
+        command = "aws cloudfront create-invalidation --distribution-id ${aws_cloudfront_distribution.s3_distribution.id} --paths '/*'"
     }
 } 
